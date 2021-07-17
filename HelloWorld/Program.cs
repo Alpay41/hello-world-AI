0494b723e0eac99b18bf906c6793387dc09e73ac
Holly Parsons <holly-parsons@outlook.com>
2021-07-17 15:32:59 -0500

    added names + colors

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,46 +8,53 @@ namespace hello_world
         static void Main(string[] args)
         {
 
-            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+            Console.WriteLine("Howdy Y'all!!");
+            Console.WriteLine("Please type your name");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+            Console.WriteLine("Please type your favorite color");
+
+            var favoriteColor = Console.ReadLine();
+
+
+            var animals = new String[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
 
             foreach (var animal in animals)
             {
-                if (animal.Length > 5)
                 {
-                    Console.WriteLine(animal);
-                }
-
-            }
-            if (args.Length == 0)
-            {
-                Console.WriteLine("nothing here");
-            }
-            else
-            {
-                Console.WriteLine("Pick your poison:");
-                for (var i = 0; i < args.Length; i++)
-                {
-                    Console.Write(args[i] + Environment.NewLine);
-                }
-                var userResponse = Console.ReadLine();
-                switch (userResponse.ToLower())
-                {
-                    case "southern":
-                        Console.WriteLine("Howdy Y'all!");
-                        break;
-                    case "meh":
-                        Console.WriteLine("hey");
-                        break;
-                    case "texas":
-                        Console.WriteLine("murrica! don't take mah guns!");
-                        break;
-                    default:
-                        Console.WriteLine("Howdy, Y'all!");
-                        break;
+                    if (animal.Length > 5)
+                    {
+                        Console.WriteLine(animal);
+                    }
                 }
             }
 
-            Console.ReadKey();
+            Random rand = new Random();
+            int index = rand.Next(animals.Length);
+            Console.WriteLine($"Would you like to have a {favoriteColor} {animals[index]}?");
+            Console.WriteLine("Pick your poison:");
+            for (var i = 0; i < args.Length; i++)
+            {
+                Console.Write(args[i] + Environment.NewLine);
+            }
+            var userResponse = Console.ReadLine();
+            switch (userResponse.ToLower())
+            {
+                case "southern":
+                    Console.WriteLine("Howdy Y'all!");
+                    break;
+                case "meh":
+                    Console.WriteLine("hey");
+                    break;
+                case "texas":
+                    Console.WriteLine("murrica! don't take mah guns!");
+                    break;
+                default:
+                    Console.WriteLine("Howdy, Y'all!");
+                    break;
+            }
         }
     }
 }
