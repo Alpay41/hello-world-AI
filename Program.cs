22a025708abb7c26f3d3b5f1085fc51a35b879b7
Holly Parsons <holly-parsons@outlook.com>
2021-07-17 12:54:41 -0500

    animals

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace hello_world
 {
@@ -6,7 +7,47 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+
+            foreach (var animal in animals)
+            {
+                if (animal.Length > 5)
+                {
+                    Console.WriteLine(animal);
+                }
+
+            }
+            if (args.Length == 0)
+            {
+                Console.WriteLine("nothing here");
+            }
+            else
+            {
+                Console.WriteLine("Pick your poison:");
+                for (var i = 0; i < args.Length; i++)
+                {
+                    Console.Write(args[i] + Environment.NewLine);
+                }
+                var userResponse = Console.ReadLine();
+                switch (userResponse.ToLower())
+                {
+                    case "southern":
+                        Console.WriteLine("Howdy Y'all!");
+                        break;
+                    case "meh":
+                        Console.WriteLine("hey");
+                        break;
+                    case "texas":
+                        Console.WriteLine("murrica! don't take mah guns!");
+                        break;
+                    default:
+                        Console.WriteLine("Howdy, Y'all!");
+                        break;
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
