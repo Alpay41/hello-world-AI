1f60f0674ff516a0badfda896e00f46996327910
Holly Parsons <holly-parsons@outlook.com>
2021-07-18 18:22:58 -0500

    added notes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,20 +8,20 @@ namespace hello_world
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Howdy Y'all!!");
+            Console.WriteLine("Howdy Y'all!!"); // greeting lines
             Console.WriteLine("what should I call you?");
 
-            var name = Console.ReadLine();
+            var name = Console.ReadLine(); // read user input
 
-            Console.WriteLine($"hey {name}");
+            Console.WriteLine($"hey {name}"); // string interpolation
             Console.WriteLine("what's your fav color?");
 
-            var favoriteColor = Console.ReadLine();
+            var favoriteColor = Console.ReadLine(); // read user input
 
 
             var animals = new String[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
 
-            foreach (var animal in animals)
+            foreach (var animal in animals) // print animals with more than 5 chars
             {
                 {
                     if (animal.Length > 5)
@@ -31,17 +31,17 @@ namespace hello_world
                 }
             }
 
-            Random rand = new Random();
-            int index = rand.Next(animals.Length);
-            Console.WriteLine($"how's about a {favoriteColor} {animals[index]}?");
+            Random rand = new Random(); // new random string
+            int index = rand.Next(animals.Length); // random selection of animals that fulfill earlier conditional
+            Console.WriteLine($"how's about a {favoriteColor} {animals[index]}?"); // prints user input + random animal
 
-            Console.WriteLine("Pick your poison:");
+            Console.WriteLine("Pick your poison:"); // new section of command line
             for (var i = 0; i < args.Length; i++)
             {
                 Console.Write(args[i] + Environment.NewLine);
             }
             var userResponse = Console.ReadLine();
-            switch (userResponse.ToLower())
+            switch (userResponse.ToLower()) // switch case for greetings, will convert chars in user input to lower case
             {
                 case "southern":
                     Console.WriteLine("Howdy Y'all!");
